54da1b5ac38b38de3a94b1182301575f487ce036
Gabriella Castro <gabicastro.pdi@gmail.com>
2021-07-12 16:21:52 -0300

    na minha cabeça está tudo pronto e funcionando lindamente 52

--- app/src/main/java/com/example/avaliacao2bimestre/MainActivity.java
+++ app/src/main/java/com/example/avaliacao2bimestre/MainActivity.java
@@ -5,15 +5,23 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
 
-    //atributos
+    //atributos - tela
     private EditText txtNome;
     private ImageView imgVolta;
+
+    private CheckBox chkPar;
+    private CheckBox chkImpar;
+    private CheckBox chkPar2;
+    private CheckBox chkImpar2;
 
     private ImageView img1;
     private ImageView img2;
@@ -28,9 +36,10 @@ public class MainActivity extends AppCompatActivity {
     private TextView lblResultVc;
     private TextView lblResultApp;
 
-
+    //atributos - código
     private String nome;
     private int escolhaUsuario = 0;
+    private boolean par = false;
 
 
     @Override
@@ -42,6 +51,11 @@ public class MainActivity extends AppCompatActivity {
 
         txtNome = findViewById(R.id.txtNome);
         imgVolta = findViewById(R.id.imgVolta);
+
+        chkPar = findViewById(R.id.chkPar);
+        chkImpar = findViewById(R.id.chkImpar);
+        chkPar2 = findViewById(R.id.chkPar2);
+        chkImpar2 = findViewById(R.id.chkImpar2);
 
         img1 = findViewById(R.id.img1);
         img2 = findViewById(R.id.img2);
@@ -56,21 +70,101 @@ public class MainActivity extends AppCompatActivity {
         lblResultVc = findViewById(R.id.lblResultVc);
         lblResultApp = findViewById(R.id.lblResultApp);
 
-        //escutadores
-
+        //escutador do botão para começar
         btnComecar.setOnClickListener(new EscutadorBotaoComecar());
 
+        // Criando um objeto escutador de cliques das imagens:
+        EscutadorClickImagem eci = new EscutadorClickImagem();
+
+        // Associando o objeto criado acima nas imagens:
+        img1.setOnClickListener( eci );
+        img2.setOnClickListener( eci );
+        img3.setOnClickListener( eci );
+        img4.setOnClickListener( eci );
+        img5.setOnClickListener( eci );
+        imgVolta.setOnClickListener( eci );
+
+        // Criando um objeto escutador de cliques dos checkboxs:
+        EscutadorCheckBox checked = new EscutadorCheckBox();
+
+        // Associando o objeto criado acima nos checkboxs:
+        chkImpar.setOnClickListener( checked );
+        chkPar.setOnClickListener( checked );
     }
 
     class EscutadorBotaoComecar implements View.OnClickListener {
         @Override
         public void onClick(View v) {
-
             nome = txtNome.getText().toString();
             if(!txtNome.getText().toString().isEmpty() && escolhaUsuario != 0 && escolhaUsuario != 6){
 
-            }
+                // bloqueamos a edição do nome
+                // pq só poderá mudar se iniciar o jogo do zero
+                txtNome.setEnabled(false);
 
+                //vamos a checkbox do app
+                if (par){
+                    //app = ímpar
+                    chkImpar2.setChecked(true);
+                    chkPar2.setChecked(false);
+                }else{
+                    //app = par
+                    chkImpar2.setChecked(false);
+                    chkPar2.setChecked(true);
+                }
+
+                // Vamos gerar aleatoriamente a escolha do app.
+                // O método nextInt(n) gera um número aleatório entre 0 e (n-1).
+                // Como queremos de 1 a 3, geramos de 0 a 2, e somamos 1.
+
+                int escolhaApp = new Random().nextInt(4) + 1;
+
+                // Precisamos colocar a imagem correta que reflete
+                // A escolha do App:
+
+                switch ( escolhaApp ) {
+                    case 1:
+                        imgApp.setImageResource(R.drawable.um);
+                        break;
+                    case 2:
+                        imgApp.setImageResource(R.drawable.dois);
+                        break;
+                    case 3:
+                        imgApp.setImageResource(R.drawable.tres);
+                        break;
+                    case 4:
+                        imgApp.setImageResource(R.drawable.quatro);
+                        break;
+                    case 5:
+                        imgApp.setImageResource(R.drawable.cinco);
+                        break;
+                }
+
+                // vamos somar as duas escolhas
+                // vemos se deu par ou ímpar
+                // vamos verificar qual foi a escolha do usuário
+                // exibir resultado
+
+                if( (escolhaApp + escolhaUsuario) % 2 == 0){
+                    //par
+                    if(par){
+                        lblResult.setText(nome + " ganhou!");
+                        lblResultVc.setText(Integer.parseInt((String) lblResultVc.getText())+1);
+                    }else{
+                        lblResult.setText(nome + ", você perdeu =(");
+                        lblResultApp.setText(Integer.parseInt((String) lblResultApp.getText())+1);
+                    }
+                }else{
+                    //ímpar
+                    if(!par){
+                        lblResult.setText(nome + " ganhou!");
+                        lblResultVc.setText(Integer.parseInt((String) lblResultVc.getText())+1);
+                    }else{
+                        lblResult.setText(nome + ", você perdeu =(");
+                        lblResultApp.setText(Integer.parseInt((String) lblResultApp.getText())+1);
+                    }
+                }
+            }
         }
     }
 
@@ -79,7 +173,6 @@ public class MainActivity extends AppCompatActivity {
     private class EscutadorClickImagem implements View.OnClickListener {
         @Override
         public void onClick(View view) {
-
             // Precisamos identificar qual imagem o usuário clicou.
             // Devemos converter View em ImageView, e depois utilizar o metodo getId().
             ImageView img = (ImageView) view;
@@ -100,51 +193,38 @@ public class MainActivity extends AppCompatActivity {
                     escolhaUsuario = 5;
                     break;
                 case R.id.imgVolta:
-                    escolhaUsuario = 6;
+                    System.exit(0);
+                    break;
+            }
+        }
+    }
+
+    // escutador das checkboxs
+    // o usuário só poderá clicar nos dois primeiros checkbox
+    private class EscutadorCheckBox implements View.OnClickListener{
+        @Override
+        public void onClick(View v) {
+            CheckBox chk = (CheckBox) v;
+
+            switch ( chk.getId() ){
+                //se selecionar PAR
+                //impar será descelecionado
+                case R.id.chkPar:
+                    chkImpar.setChecked(false);
+                    //o boolean 'par' recebe true
+                    //quando par for selecionado
+                    par = true;
+                    break;
+
+                //se selecionar IMPAR
+                //par será descelecionado
+                case R.id.chkImpar:
+                    chkImpar.setChecked(true);
+                    //o boolean 'par' recebe false
+                    //quando impar for selecionado
+                    par = false;
                     break;
             }
         }
     }
 }
-//----------------------------------------------------------------------------------------------------------------------------------------------
-//            // Vamos gerar aleatoriamente a escolha do app.
-//            // O método nextInt(n) gera um número aleatório entre 0 e (n-1).
-//            // Como queremos de 1 a 3, geramos de 0 a 2, e somamos 1.
-//            // OBS: Isso é Java básico!!!
-//            int escolhaApp = new Random().nextInt(3) + 1;
-//            // Precisamos colocar a imagem correta que reflete
-//            // A escolha do App:
-//            switch ( escolhaApp ) {
-//                case 1:
-//                    imgApp.setImageResource(R.drawable.pedra);
-//                    break;
-//                case 2:
-//                    imgApp.setImageResource(R.drawable.papel);
-//                    break;
-//                case 3:
-//                    imgApp.setImageResource(R.drawable.tesoura);
-//                    break;
-//            }
-////----------------------------------------------------------------------------
-//            // Vamos ver quem ganhou... e informar o resultado.
-//            if ( ( escolhaApp == 1 && escolhaUsuario == 3 ) ||
-//                    ( escolhaApp == 2 && escolhaUsuario == 1 ) ||
-//                    ( escolhaApp == 3 && escolhaUsuario == 2 ) )
-//            {
-//                lblResultado.setText("O app ganhou!!!!");
-//            }
-//            else
-//            {
-//                if ( (escolhaApp == 3 && escolhaUsuario == 1) ||
-//                        (escolhaApp == 1 && escolhaUsuario == 2) ||
-//                        (escolhaApp == 2 && escolhaUsuario == 3) )
-//                {
-//                    lblResultado.setText("Você ganhou!!");
-//                }
-//                else
-//                {
-//                    lblResultado.setText("Deu empate!");
-//                }
-//            }
-//        }
-//    }
